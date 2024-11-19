e9af047d39fef8240ff1b50978d447344ac7f0f3
Doroti3434 <dashkatoporovskay13@gmail.com>
2024-11-19 17:35:42 +0300

    added login screen and modified also have bag with font and icons on login/registration screens  ~23 minutes

---
+++ app/src/main/java/com/dorinidelishki/test_work/LoginActivity.kt
@@ -0,0 +1,44 @@
+package com.dorinidelishki.test_work
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import com.dorinidelishki.test_work.databinding.ActivityLoginBinding
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
+
+
+
+class LoginActivity: Activity() {
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.regTV.setOnClickListener {
+            val intent = Intent(this, RegistrationActivity::class.java)
+            startActivity(intent)
+        }
+
+        var isPasswordVisible = false
+
+        binding.eyeIV.setOnClickListener {
+            isPasswordVisible = !isPasswordVisible
+            if (isPasswordVisible) {
+                binding.loginPasswordET.transformationMethod =
+                    HideReturnsTransformationMethod.getInstance()
+                binding.eyeIV.setImageResource(R.drawable.ic_eye_open)
+            } else {
+                binding.loginPasswordET.transformationMethod =
+                    PasswordTransformationMethod.getInstance()
+                binding.eyeIV.setImageResource(R.drawable.ic_eye_closed)
+             }
+             binding.loginPasswordET.setSelection(binding.loginPasswordET.text.length)
+        }
+
+    }
+
+}
