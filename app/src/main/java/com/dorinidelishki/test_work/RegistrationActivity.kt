08a47954bc5ce94611b0b0a986181f80a3c57c3d
Doroti3434 <dashkatoporovskay13@gmail.com>
2024-11-19 17:34:38 +0300

    added login screen and modified also have bag with font and icons on login/registration screens  ~23 minutes

--- app/src/main/java/com/dorinidelishki/test_work/RegistrationActivity.kt
+++ app/src/main/java/com/dorinidelishki/test_work/RegistrationActivity.kt
@@ -2,10 +2,14 @@ package com.dorinidelishki.test_work
 
 import android.app.Activity
 import android.os.Bundle
-import android.os.PersistableBundle
+import com.dorinidelishki.test_work.databinding.ActivityRegistrationBinding
 
 class RegistrationActivity: Activity() {
+    private lateinit var binding: ActivityRegistrationBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityRegistrationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/dorinidelishki/test_work/fragments/ThirdFragment.kt
+++ app/src/main/java/com/dorinidelishki/test_work/fragments/ThirdFragment.kt
@@ -3,6 +3,7 @@ package com.dorinidelishki.test_work.fragments
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.dorinidelishki.test_work.R
 import com.dorinidelishki.test_work.databinding.FragmentThirdBinding
 
@@ -13,6 +14,10 @@ class ThirdFragment: Fragment(R.layout.fragment_third) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentThirdBinding.bind(view)
+
+        binding?.letsGoButton?.setOnClickListener {
+            findNavController().navigate(ThirdFragmentDirections.actionThirdToLogin())
+        }
     }
 
     override fun onDestroyView() {
